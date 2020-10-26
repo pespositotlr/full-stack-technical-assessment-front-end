92bcef3f8e64665ae18cbfdc123f5ce735d2d34a
pespositotlr <pespositotlr@gmail.com>
2020-10-25 20:43:11 -0400

    Update tests

--- src/tests/repositoryactions.test.js
+++ src/tests/repositoryactions.test.js
@@ -3,21 +3,25 @@ import thunk from 'redux-thunk';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import * as repositoryActions from '../store/actions/repositoryactions'
-import * as repositoryReducer from '../store/reducers/repositoryreducer'
+const mock = new MockAdapter(axios);
+
 const middleware = [thunk];
 const mockStore = configureMockStore(middleware);
-const mock = new MockAdapter(axios);
 const store = mockStore();
-
-const baseUrl = 'https://localhost:44346';
 
 describe('Repository actions', () => {
 
-    beforeEach(() => { store.clearActions(); });
+    beforeEach(() => {
+        store.clearActions();
+    });
     
     it('performs getData request', () => {
-            let props = {};
-            store.dispatch(repositoryActions.getData('api/GetItems', props))
+        const props = { };
+        const items = [];
+
+        store.dispatch(repositoryActions.getData('api/GetItems', props));
+        expect(store.getActions()).toEqual(items);
+
     });	
 
     it('performs postData request', () => {
